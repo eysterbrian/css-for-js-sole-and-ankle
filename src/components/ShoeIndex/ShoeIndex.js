f10700ac87bb8b1da9bb3648c68ab97ee0560466
Brian Eyster <eysterbrian@gmail.com>
2021-05-03 17:39:41 -0600

    Ex3: Main Shell
    Uses width of 250px for LeftColumn with no
    flex/shrink.

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -18,22 +18,19 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           <Select
             label="Sort"
             value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
+            onChange={(ev) => setSortId(ev.target.value)}>
             <option value="newest">Newest Releases</option>
             <option value="price">Price</option>
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
           <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
@@ -42,13 +39,33 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 32;
+  justify-content: flex-end;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  padding-top: 14px; /* HACK: This value taken via optical alignment */
+  flex: 0 0 248px; /* Width taken from Figma file */
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  gap: 32px;
+
+  label {
+    display: flex;
+    align-items: baseline;
+  }
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
