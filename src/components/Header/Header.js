783f768b537f31f1925f3059a975b480451df86f
Brian Eyster <eysterbrian@gmail.com>
2021-05-03 16:55:20 -0600

    Ex2: Header
    Initial version only reponsive width in the
    margin between the logo and the NavLink items.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,20 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 24px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
+  gap: 24px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin-left: auto;
+  margin-right: auto;
+  flex-shrink: 1;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
